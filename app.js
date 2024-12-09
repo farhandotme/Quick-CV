10f7c137a2c3e7261a7a3ea2877514ce43e4b992
farhandotme <princeofficial355@gmail.com>
2024-12-09 22:58:44 +0530

    organized

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectionDb = require("./DB/connectionDb");
+connectionDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Quick CV application listening on port ${port}`);
+});
